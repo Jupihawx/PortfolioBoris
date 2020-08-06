ba525f21be7a67a3a0bcf474337fe03f9de240c5
Jupihawx <borishuljak@hotmail.fr>
2020-08-06 12:33:46 +0200

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 require(`dotenv`).config({
   path: `.env`,
-})
+});
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Boris Huljak Portfolio`,
   },
   plugins: [
     {
@@ -20,29 +20,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: "UA-168825665-3",
       },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
-        start_url: `/`,
+        name: `Boris Huljak`,
+        short_name: `Boris`,
+        description: `Playful and Colorful portfolio featuring Parallax effects and animations`,
+        start_url: `/portfolio`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
+            src: `/favicon.ico`,
             sizes: `512x512`,
-            type: `image/png`,
+            type: `image/ico`,
           },
         ],
       },
@@ -58,4 +53,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
